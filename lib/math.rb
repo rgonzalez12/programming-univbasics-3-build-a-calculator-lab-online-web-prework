31dc40a12bdf72cfb05ed534dcb75b5d79f149a2
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-08-27 04:29:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,23 +2,34 @@ def addition(a,b)
 5 + 4
 end
 
+addition
+
 def subtraction(num1, num2)
 10 - 5
 end
+
+subtraction
 
 def division(num1, num2)
 50 / 2
 end
 
+division
+
 def multiplication(num1, num2)
 4 * 30
 end
 
+multiplication
+
 def modulo(num1, num2).to_s
 34 / 5
-
 end
+
+modulo
 
 def square_root(num)Math.sqrt
 81
 end
+
+square_root
